c91e9b6158f894bd5e37e4c250151e7f05a083c2
Victor Santos <victor.santoss090@gmail.com>
2025-08-26 12:44:23 -0300

    fix

--- ticto/src/app/page.tsx
+++ ticto/src/app/page.tsx
@@ -22,8 +22,8 @@ export default function Home() {
     <div>
       <header className="header">
         <style>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap");
 </style>
         <h1 className = "title">TICTO</h1>
         <button onClick={() => setIsModalOpen(true)}>NOVA TRANSAÇÃO</button>
